c46322172ce9cba1d4ace1b2a1d6008eb0c6c7c6
ektaleshina <158627674+ektaleshina@users.noreply.github.com>
2024-09-27 02:10:44 +0300

    Update funcc.js

--- funcc.js
+++ funcc.js
@@ -6,7 +6,7 @@ button.addEventListener('click', () => {
         .then(response => {
       
             if (!response.ok) {
-                throw new Error('Сеть ответила с ошибкой: ' + response.status);
+                throw new Error('ERROR: ' + response.status);
             }
             return response.json(); // 
         })
